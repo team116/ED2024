fddbf2420a83225de3401a9ad46abf9e00a6ab7d
ftc519 <ftc519@gmail.com>
2024-02-27 20:17:11 -0500

    Remove a now incorrect and unnecessary comment

--- src/main/java/frc/robot/RobotSpecificConstants.java
+++ src/main/java/frc/robot/RobotSpecificConstants.java
@@ -60,7 +60,7 @@ public final class RobotSpecificConstants {
     private static final class TestRobot implements SpecificConstants {
         public double getFrontToBackAxleToAxleInches() { return 27.25; }
         public double getSideToSideTreadCenterToTreadCenterInches() { return 27.25; }
-        public double getAngleOffsetDegreesMod0() { return 110.302734375; } //Mod0 had issues at the time, and as such I didn't find an appropriate encoder offset
+        public double getAngleOffsetDegreesMod0() { return 110.302734375; }
         public double getAngleOffsetDegreesMod1() { return 173.49609375; }
         public double getAngleOffsetDegreesMod2() { return 62.75390625; }
         public double getAngleOffsetDegreesMod3() { return 21.533203125; }
