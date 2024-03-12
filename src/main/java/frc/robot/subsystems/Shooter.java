6910f50c6e29cf763da9e1cd6017a9a61544662e
ftc519 <ftc519@gmail.com>
2024-03-11 21:34:00 -0400

    shooter speed chaos

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,11 +12,40 @@ public class Shooter extends SubsystemBase {
     private CANSparkMax shooterMotor1;  // TODO: Might be "closer"/"further", find out when installed
     private CANSparkMax shooterMotor2;
 
+    private RelativeEncoder shooter1Encoder;
+    private RelativeEncoder shooter2Encoder;
+
     public Shooter() {
         shooterMotor1 = new CANSparkMax(Constants.SHOOTER_MOTOR_1_ID, MotorType.kBrushless);
         shooterMotor2 = new CANSparkMax(Constants.SHOOTER_MOTOR_2_ID, MotorType.kBrushless);
 
         shooterMotor1.setIdleMode(IdleMode.kCoast);
         shooterMotor2.setIdleMode(IdleMode.kCoast);
+
+        shooterMotor2.setInverted(true);
+
+        shooter1Encoder = shooterMotor1.getEncoder();
+        shooter2Encoder = shooterMotor2.getEncoder();
+    }
+
+    public double getMotor1Velocity() {
+        return shooter1Encoder.getVelocity();
+    }
+
+    public double getMotor2Velocity() {
+        return shooter2Encoder.getVelocity();
+    }
+
+    public void setMotor1Power(double percentagePower) {
+        shooterMotor1.set(percentagePower);
+    }
+
+    public void setMotor2Power(double percentagePower) {
+        shooterMotor2.set(percentagePower);
+    }
+
+    public void stop() {
+        shooterMotor1.set(0.0);
+        shooterMotor2.set(0.0);
     }
 }
